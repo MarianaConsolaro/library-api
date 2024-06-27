fcced35dd99edbb1a7070172abce896cc837d34a
Mariana <mariana.perez.consolaro@gmail.com>
2024-06-27 15:15:58 -0300

    Lesson 7 - JUnit and First Test

--- library/src/test/java/com/library/PrimeiroTeste.java
+++ library/src/test/java/com/library/PrimeiroTeste.java
@@ -1,4 +1,22 @@
 package com.library;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PrimeiroTeste {
+    @Test
+    public void estruturaDeUmTeste(){
+
+        //cenário do teste
+        int numero1 = 10, numero2 = 5;
+
+        //execução do teste
+        int resultado = numero1 + numero2;
+
+        //verificações
+        Assert.assertEquals(15, resultado);
+
+    }
+
 }
